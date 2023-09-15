f296e11f407ae01e91e5cb778b23c8480f253aae
Jyrki De Neve <jyrki.deneve@gmail.com>
2023-09-15 14:35:04 +0300

    Show bought items from last 24 hours as well in the list detail

--- pages/lists/[id].vue
+++ pages/lists/[id].vue
@@ -40,7 +40,6 @@
               <input
                 type="text"
                 :value="element.name"
-                :class="{ 'line-through': element.is_bought }"
                 @change="updateItemName(element.id, $event.target.value)"
                 class="outline-none grow bg-transparent border-0 p-2"
               />
@@ -55,13 +54,38 @@
             </div>
           </template>
         </Sortable>
+        <div
+            class="divide-y divide-red-100"
+        >
+          <template v-for="element in boughtItems" :key="element.id">
+            <div class="draggable flex py-2 items-center w-full">
+              <span class="p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"></svg>
+              </span>
+
+              <input
+                  type="text"
+                  :value="element.name"
+                  @change="updateItemName(element.id, $event.target.value)"
+                  class="outline-none grow bg-transparent border-0 p-2 line-through"
+              />
+
+              <input
+                  type="checkbox"
+                  class="appearance-none checked:bg-blue-500 h-5 w-5 border-2 rounded-full p-2"
+                  @change="toggleIsBought(element.id, element.is_bought)"
+                  :checked="element.is_bought"
+              />
+            </div>
+          </template>
+        </div>
       </div>
   </div>
 </template>
 
 <script setup>
 import { useDocument } from 'vuefire'
-import { collection, doc, updateDoc, addDoc, orderBy, where, query, writeBatch } from 'firebase/firestore'
+import { collection, doc, updateDoc, addDoc, orderBy, where, query, writeBatch, or, and, Timestamp } from 'firebase/firestore'
 import { Sortable } from 'sortablejs-vue3'
 
 const state = reactive({ newItemName: '' })
@@ -110,8 +134,25 @@ watch(list, async (newList, oldList) => {
   }
 })
 
+// is_bought = false
+// OR
+// is_bought = true AND updated_at = null OR updated_at <= now + 24h
 const itemsRef = collection(firestore, 'lists', route.params.id, 'items')
-const items = useCollection(query(itemsRef, where("is_bought", "==", false), orderBy("sort_order", "desc")))
+const items = useCollection(query(
+    itemsRef,
+    where("is_bought", "==", false),
+    orderBy("sort_order", "desc")
+))
+
+const boughtItems = useCollection(query(
+    itemsRef,
+    and(
+      where("is_bought", "==", true),
+      where('updated_at', '>=', Timestamp.fromMillis(
+          new Date().getTime() - 86400000
+      )),
+    ),
+))
 
 const updateTitle = async (title) => {
   await updateDoc(listRef, {
@@ -123,7 +164,8 @@ const addItem = async () => {
   await addDoc(itemsRef, {
     name: state.newItemName,
     is_bought: false,
-    sort_order: items.value.length + 1
+    sort_order: items.value.length + 1,
+    updated_at: Timestamp.now(),
   });
 
   state.newItemName = ''
@@ -137,7 +179,8 @@ const updateItemName = async (id, name) => {
 
 const toggleIsBought = async (id, currentValue) => {
   await updateDoc(doc(itemsRef, id), {
-      is_bought: ! currentValue
+    is_bought: ! currentValue,
+    updated_at: Timestamp.now(),
   });
 }
 
